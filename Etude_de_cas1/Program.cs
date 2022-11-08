b69334f0a9b3d295c335f25f7104af2ab89f5c9f
HP <HP@DESKTOP-RG3QUVA>
2022-11-08 08:18:50 +0100

    Add project files.

---
+++ Etude_de_cas1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etude_de_cas1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
